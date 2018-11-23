48e2e5992717cb74fb05b18d46457a49b92031e9
ivan rey c.v <ivan.rey.c.v@gmail.com>
2018-11-23 20:34:19 +0800

    Add gatsby config meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Ivan Rey C.V. Portfolio',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Portfolio website of Ivan Rey C.V.',
+        short_name: 'ircv portfolio',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
