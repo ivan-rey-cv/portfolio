d0e11484021ff88df0a0c3654224f315dcbebc58
ivan rey c.v <ivan.rey.c.v@gmail.com>
2018-11-23 16:15:30 +0800

    Revert from using react lazy and Suspense for error in react minification

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,18 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 
 import MainLayout from '../layouts/MainLayout'
-import Loading from '../components/Loading'
-
-const AppHeader = lazy(e => import('../components/AppHeader'))
-const FrontTitle = lazy(e => import('../components/front-title/FrontTitle'))
-const ProjectsSection = lazy(e =>
-  import('../components/projects/ProjectsSection')
-)
-const About = lazy(e => import('../components/about/About'))
+import AppHeader from '../components/AppHeader'
+import FrontTitle from '../components/front-title/FrontTitle'
+import ProjectsSection from '../components/projects/ProjectsSection'
+import About from '../components/about/About'
 
 function IndexPage() {
   return (
     <MainLayout>
-      <Suspense fallback={<Loading />}>
-        <AppHeader />
-        <FrontTitle />
-        <ProjectsSection />
-        <About />
-      </Suspense>
+      <AppHeader />
+      <FrontTitle />
+      <ProjectsSection />
+      <About />
     </MainLayout>
   )
 }
