ff4bb2fbdb3c3258ba03c15b3f9686a10bac016c
Ivan Rey C.V <ivan.rey.c.v@gmail.com>
2019-08-13 16:36:26 +0800

    Integrate circle CI
    Add dependency:firebase-tools needed for firebase deployment.
    Add mocks for gatsby itself and add pages tests.

--- src/__tests__/Root.test.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const { render } = require('@testing-library/react');
-
-const { wrapRootElement: Root } = require('../../gatsby-browser');
-
-test('should load the gatsby root element', () => {
-    render(<Root />);
-});
